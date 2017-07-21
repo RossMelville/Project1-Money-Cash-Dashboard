99c5d57f30aa564895465450ab80df9ac5a3756a
Ross Melville <rossmelville@hotmail.com>
2017-07-21 14:01:26 +0100

    Seeds.rb file created with required files added.

--- models/transaction.rb
+++ models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction
 
   def initialize (options)
     @id = options['id'].to_i if options['id']
-    @value = options['value'].to_f
+    @value = options['value'].to_f.round(2)
     @merchant_id = options['merchant_id'].to_i
     @tag_id = options['tag_id'].to_i
   end
