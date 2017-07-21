eee95afb43f4bb99e59417f383426767f7ad8ef0
Ross Melville <rossmelville@hotmail.com>
2017-07-21 13:14:07 +0100

    Initial commit. Money database created and create tables added to money.sql file.

---
+++ db/money.sql
@@ -0,0 +1,24 @@
+DROP TABLE transactions;
+DROP TABLE merchants;
+DROP TABLE tags;
+
+CREATE TABLE tags 
+(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  icon VARCHAR(255)  
+);
+
+CREATE TABLE merchants
+(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE transactions
+(
+  id SERIAL8 PRIMARY KEY,
+  value INT4,
+  merchant_id INT8 REFERENCES merchants(id),
+  tag_id INT8 REFERENCES tags(id)
+);
